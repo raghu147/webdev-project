f415e3a6453b399731d423b6b54716e3a9fab269
Raghuveer Ramesh <raghuveer.ramesh@yahoo.com>
2016-12-10 15:25:16 -0500

    Modifying schema for details

--- server/models/concert/concert.schema.server.js
+++ server/models/concert/concert.schema.server.js
@@ -5,11 +5,13 @@ module.exports = function() {
         cid: String,
         users: [{type:mongoose.Schema.Types.ObjectId, ref:'UserModel'}],
         title: String,
-        venue: String,
-        datetime: String,
+        venue_name: String,
+        date: String,
+        time: String,
         artist: String,
-        imageUrl: String,
-        city: String,
+        imageURL: String,
+        description: String,
+        address:String,
         dateCreated: {type: Date, default: Date.now}
     }, {collection: "concert"});
 
